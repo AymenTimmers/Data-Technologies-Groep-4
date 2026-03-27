47d6777990ad7aa24aca1955fa76ca4ea03eeb2e
jurvdw <jurvdw123@gmail.com>
2026-03-27 14:18:22 +0100

    final push -->

--- WebShop.Api.Tests/obj/Debug/net8.0/WebShop.Api.Tests.AssemblyInfo.cs
+++ WebShop.Api.Tests/obj/Debug/net8.0/WebShop.Api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebShop.Api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da4b0fd887dcd8281b1e572aadc32c49b234d2d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c03875a519599ab3d135d0fc30c5c500180d444")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebShop.Api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebShop.Api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
