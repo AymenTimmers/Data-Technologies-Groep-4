33776e71e0ca8c7bf792d4153b4b0e076fe493bf
ali7682 <alialimail2003@gmail.com>
2026-05-08 01:58:21 +0200

    Git trackt automatisch gegenereerde bestanden niet meer

--- WebShop.Api.Tests/bin/Debug/net8.0/Database/Models/09_payments.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE payments (
-    id INTEGER PRIMARY KEY,
-    order_id INTEGER NOT NULL,
-    transaction_reference TEXT NOT NULL UNIQUE,
-    total_paid REAL NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(id)
-);
