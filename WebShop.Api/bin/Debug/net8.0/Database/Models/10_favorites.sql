f16295d8324b89775fdc032f573797164bdfd71a
ali7682 <alialimail2003@gmail.com>
2026-05-08 06:15:11 +0200

    gitignore

--- WebShop.Api/bin/Debug/net8.0/Database/Models/10_favorites.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE favorites (
-    id INTEGER PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
-    UNIQUE (user_id, product_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (product_id) REFERENCES products(id)
-);
